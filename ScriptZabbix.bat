79c9ecc9e185c9e83f23c0a1b7d7c03a7a026145
Antonetwork <Pardoytmc@gmail.com>
2024-09-09 11:12:29 +0200

    Update ScriptZabbix.bat

--- ScriptZabbix.bat
+++ ScriptZabbix.bat
@@ -1,21 +1,21 @@
 @echo off
 
-REM Définit le chemin du fichier MSI
-set "MSI_PATH=\\192.168.13.123\Users\Administrateur\Desktop\Partage\zabbix_agent-7.0.3-windows-amd64-openssl (1).msi"
+REM Define the path to the MSI file
+set "MSI_PATH=\\path\to\your\file\zabbix_agent-7.0.3-windows-amd64-openssl.msi"
 
-REM Définit les paramètres d'installation personnalisés
-set ZABBIX_SERVER=192.168.13.119
+REM Define custom installation parameters
+set ZABBIX_SERVER=your_zabbix_server_address
 set AGENT_HOSTNAME=%COMPUTERNAME%
-set HOST_METADATA=Debian
-set TLSPSK_IDENTITY=Zabbix
+set HOST_METADATA=your_custom_metadata
+set TLSPSK_IDENTITY=your_psk_identity
 
-REM Génère la clé PSK à l'aide du script PowerShell
-for /f "delims=" %%i in ('powershell -ExecutionPolicy Bypass -File "\\192.168.13.123\Users\Administrateur\Desktop\Partage\GeneratePSK.ps1"') do set "TLSPSK_VALUE=%%i"
+REM Generate the PSK key using the PowerShell script
+for /f "delims=" %%i in ('powershell -ExecutionPolicy Bypass -File "\\path\to\your\script\GeneratePSK.ps1"') do set "TLSPSK_VALUE=%%i"
 
-REM Affiche la clé PSK générée pour vérification
-echo La clé PSK générée est : %TLSPSK_VALUE%
+REM Display the generated PSK key for verification
+echo The generated PSK key is: %TLSPSK_VALUE%
 
-REM Installe l'agent Zabbix avec les paramètres
+REM Install the Zabbix agent with the parameters
 msiexec /i "%MSI_PATH%" /quiet /norestart /log "%temp%\zabbix_agent_install.log" ^
 ENABLEREMOTECOMMANDS=1 ^
 SERVER=%ZABBIX_SERVER% ^
@@ -27,20 +27,20 @@ TLSACCEPT=psk ^
 TLSPSKIDENTITY=%TLSPSK_IDENTITY% ^
 TLSPSKVALUE=%TLSPSK_VALUE%
 
-REM Vérifie le statut de l'installation
+REM Check the installation status
 if %ERRORLEVEL% neq 0 (
-    echo Une erreur est survenue pendant l'installation.
+    echo An error occurred during installation.
     pause
 )
 
-REM Configure le service Zabbix pour démarrer automatiquement
+REM Configure the Zabbix service to start automatically
 sc config "Zabbix Agent" start= auto
 
-REM Attendre 20 secondes avant de démarrer le service
+REM Wait 20 seconds before starting the service
 timeout /t 20 /nobreak
 
-REM Démarre le service Zabbix Agent
+REM Start the Zabbix Agent service
 sc start "Zabbix Agent"
 
-REM Empêche la fermeture automatique de la fenêtre CMD
+REM Prevent the CMD window from closing automatically
 pause
